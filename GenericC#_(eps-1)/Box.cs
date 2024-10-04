b8c491b4fd1300356f1703609537e39d21fbedab
alif.meydi <alif.meydi@rds.co.id>
2024-10-04 11:10:45 +0700

    feat: first commit

---
+++ GenericC#_(eps-1)/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericC___eps_1_
+{
+    internal class Box<T>
+    {
+        public T Content { get; set; }
+
+        public string Log()
+        {
+            return $"Ini box isinya: {Content}";
+        }
+    }
+}
---
+++ GenericC#_(eps-1)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericC___eps_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> boxInt = new Box<int>();
+            boxInt.Content = 5;
+            Console.WriteLine(boxInt.Log());
+
+            Box<string> boxString = new Box<string>();
+            boxString.Content = "HIAAAA";
+            Console.WriteLine(boxString.Log());
+        }
+    }
+}
